8d05092a9b52b70f28071594b715df85c63bdd9c
sopheak tuon <tuon.sopheake2009570@gmail.com>
2018-01-29 08:45:20 +0700

    - Set up video 360 sample

---
+++ app/src/main/java/com/android/pathmazing/video360sample/MainActivity.java
@@ -0,0 +1,43 @@
+package com.android.pathmazing.video360sample;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import java.io.File;
+
+/**
+ * @author Sopheak Tuon
+ * @created on 29-Jan-18
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickVideo360(View view){
+        Intent intent = new Intent(MainActivity.this, VideoActivity.class);
+        File dir = Environment.getExternalStorageDirectory();
+        File yourFile = new File(dir, "/sample360.mp4");
+        Uri uri = Uri.fromFile(yourFile);
+        intent.setData(uri);
+        startActivity(intent);
+
+    }
+
+    public void onClickVideoVR(View view){
+        Intent intent = new Intent(MainActivity.this, VrVideoActivity.class);
+        File dir = Environment.getExternalStorageDirectory();
+        File yourFile = new File(dir, "/sample360.mp4");
+        Uri uri = Uri.fromFile(yourFile);
+        intent.setData(uri);
+        startActivity(intent);
+    }
+}
